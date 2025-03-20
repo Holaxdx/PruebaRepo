ccbad2d662a2197b679c8bd5f54fc8b6ac6568e9
Holaxdx <113891080+Holaxdx@users.noreply.github.com>
2025-03-20 07:05:17 -0300

    Update questions.py
    "se usa questoin_to_ask"

--- questions.py
+++ questions.py
@@ -25,43 +25,48 @@ answers = [
 # Índice de la respuesta correcta para cada pregunta, el el mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
 
-# creo la variable score para ir contando los puntos
 score = 0
 
+questions_to_ask = random.choices(list(zip(questions,answers, correct_answers_index)), k=3)
 # El usuario deberá contestar 3 preguntas
-for _ in range(3):
+
+for quest in questions_to_ask:
     # Se selecciona una pregunta aleatoria
-    question_index = random.randint(0, len(questions) - 1)
+    #question_index = random.randint(0, len(questions) - 1)
 
     # Se muestra la pregunta y las respuestas posibles
-    print(questions[question_index])
-    for i, answer in enumerate(answers[question_index]):
-        print(f"{i + 1}. {answer}")
+    
+    print(quest[0])
+    for i in range(4):
+        print(f"{i+1}. {quest[1][i]}")
 
     # El usuario tiene 2 intentos para responder correctamente
     for intento in range(2):
         user_answer = input("Respuesta: ")
         
         # Verifico que la respuesta ingresada es valida en los rangos aceptables
-        if (user_answer.isnumeric()) and (int(user_answer) < 5):
-          # Se verifica si la respuesta es correcta
-          if (int(user_answer) - 1) == correct_answers_index[question_index]:
-              print("¡Correcto!")
-              score += 1
-              break
-          else:
-              score -= 0.5
+        if (user_answer.isnumeric()) and ((int(user_answer)>0) and (int(user_answer) < 5)):    
+            # Se verifica si la respuesta es correcta
+            if (int(user_answer) - 1) == quest[2]:
+                print("¡Correcto!")
+                score += 1
+                break
+            else:
+                score-=0.5
         else:
-          print("Respuesta no válida")
-          sys.exit(1)
-          
+            print("Respuesta no válida")
+            sys.exit(1)
     else:
         # Si el usuario no responde correctamente después de 2 intentos,
         # se muestra la respuesta correcta
         print("Incorrecto. La respuesta correcta es:")
-        print(answers[question_index][correct_answers_index[question_index]])
+        print(quest[1][quest[2]])
 
+    # no sabia que el for podia tener un else, tremendo
+    
+    
+    
     # Se imprime un blanco al final de la pregunta
     print()
-
+    
 print(f"Puntaje final: {score}")
